5e2ee927acf2673c54d8bd49d7f6c08e88c48e0e
David Seow <david.seow@theculturetrip.com>
2021-01-06 17:55:22 +0000

    Update gallery photos and randomise order

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,43 +1,38 @@
 import ImageMasonry from "react-image-masonry";
+import { shuffle } from "../../lib/shuffle";
+
+const imagesPath = [
+  "./assets/photos/DSC01435.jpg",
+  "./assets/photos/DSCF0469.jpg",
+  "./assets/photos/DSCF1264.jpg",
+  "./assets/photos/DSCF3526.jpg",
+  "./assets/photos/DSCF5285.jpg",
+  "./assets/photos/DSCF5552.jpg",
+  "./assets/photos/DSCF5848.jpg",
+  "./assets/photos/DSCF7549.jpg",
+  "./assets/photos/DSCF7599.jpg",
+  "./assets/photos/DSCF7608.jpg",
+  "./assets/photos/DSCF7961.jpg",
+  "./assets/photos/IMG_0159.jpg",
+  "./assets/photos/IMG_0399.jpg",
+  "./assets/photos/IMG_0404.jpg",
+  "./assets/photos/IMG_0556.jpg",
+  "./assets/photos/IMG_1096.jpg",
+  "./assets/photos/IMG_2327.jpg",
+  "./assets/photos/IMG_2352.jpg",
+  "./assets/photos/IMG_2866.jpg",
+  "./assets/photos/IMG_2874.jpg",
+  "./assets/photos/IMG_3063.jpg",
+  "./assets/photos/IMG_3384.jpg",
+  "./assets/photos/IMG_3488.jpg",
+  "./assets/photos/IMG_3557.jpg",
+  "./assets/photos/IMG_3627.jpg",
+  "./assets/photos/IMG_5387.jpg",
+];
 
 const Gallery = () => (
   <ImageMasonry
-    imageUrls={[
-      "./assets/photos/DSCF0469.jpg",
-      "./assets/photos/DSCF0484.jpg",
-      "./assets/photos/DSCF1264.jpg",
-      "./assets/photos/DSCF5284.jpg",
-      "./assets/photos/DSCF5285.jpg",
-      "./assets/photos/DSCF5296.jpg",
-      "./assets/photos/DSCF5468.jpg",
-      "./assets/photos/DSCF5522.jpg",
-      "./assets/photos/DSCF5552.jpg",
-      "./assets/photos/DSCF5553.jpg",
-      "./assets/photos/DSCF5555.jpg",
-      "./assets/photos/DSCF5561.jpg",
-      "./assets/photos/DSCF5576.jpg",
-      "./assets/photos/DSCF5848.jpg",
-      "./assets/photos/DSCF6198.jpg",
-      "./assets/photos/DSCF7549.jpg",
-      "./assets/photos/DSCF7606.jpg",
-      "./assets/photos/DSCF7608.jpg",
-      "./assets/photos/DSCF7926.jpg",
-      "./assets/photos/DSCF7961.jpg",
-      "./assets/photos/IMG_0399.jpg",
-      "./assets/photos/IMG_0404.jpg",
-      "./assets/photos/IMG_0544.jpg",
-      "./assets/photos/IMG_0556.jpg",
-      "./assets/photos/IMG_1096.jpg",
-      "./assets/photos/IMG_2350.jpg",
-      "./assets/photos/IMG_2621.jpg",
-      "./assets/photos/IMG_2866.jpg",
-      "./assets/photos/IMG_2874.jpg",
-      "./assets/photos/IMG_2926.jpg",
-      "./assets/photos/IMG_3063.jpg",
-      "./assets/photos/IMG_3488.jpg",
-      "./assets/photos/IMG_4643.jpg",
-      "./assets/photos/IMG_5387.jpg",
-    ]}
+    imageUrls={shuffle(imagesPath)}
     numCols={3}
     containerWidth={"100%"}
   />
