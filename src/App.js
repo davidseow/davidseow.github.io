be89255f72ff35bf455cae2f4154ad68bf1609f9
David Seow <david.seow@theculturetrip.com>
2021-01-06 18:12:33 +0000

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navigation from "./components/navigation/navigation";
 import Gallery from "./components/gallery/gallery";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Gallery />
     </div>
   );
---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,19 @@
+import "./style.css";
+
+const Navigation = () => (
+  <navigation>
+    <ul>
+      <li>
+        <a href="https://www.instagram.com/davidseow/">Instagram</a>
+      </li>
+      <li>
+        <a href="https://github.com/davidseow">Github</a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/seowdavid/">LinkedIn</a>
+      </li>
+    </ul>
+  </navigation>
+);
+
+export default Navigation;
