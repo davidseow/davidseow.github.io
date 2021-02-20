3a7b4f7220cadaa81d92ac4d4f69ef7cec98dd50
David Seow <david.seow@theculturetrip.com>
2021-02-20 15:40:00 +0000

    Start migration to Gatsby..

--- README.md
+++
@@ -1,1 +0,0 @@
-# davidseow.github.io
--- src/components/gallery/__tests__/gallery.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Gallery from "../gallery";
-
-describe("Gallery", () => {
-  it("should render", () => {
-    const { asFragment } = render(<Gallery />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
