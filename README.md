ed847e5317cf38b02789cf088474890ac023e332
David Seow <david.seow@gmail.com>
2022-07-25 11:30:29 +0100

    Add masonry gallery

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # davidseow.github.io
+
+## TODO
+
+- [x] setup masonry
+- [ ] setup and unit tests
+- [ ] tweak masonry style and add shuffle
+- [ ] setup markdown dynamic pages
+- [ ] setup and verify google analytics
+- [ ] write blogs
+- [ ] post new pics
+- [ ] update code to retrieve pics from somewhere
+- [ ]
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import "./Layout.css";
+// import InstagramIcon from "../../static/icons/instagram.svg";
+// import GithubIcon from "../../static/icons/github.svg";
+
+const Layout = ({ children }) => (
+  <>
+    <div className="container">
+      <header>
+        <nav>
+          <div className="menu">
+            <Link activeClassName="active" to="/">
+              Home
+            </Link>
+            <Link activeClassName="active" to="/posts">
+              Posts
+            </Link>
+            <div className="social">
+              <a
+                href="https://www.instagram.com/davidseow/"
+                className="instagram"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Instagram - David Seow"
+              >
+                {/* <InstagramIcon /> */}
+              </a>
+              <a
+                className="github"
+                href="https://github.com/davidseow"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Github - David Seow"
+              >
+                {/* <GithubIcon /> */}
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </div>
+
+    <footer>
+      <p>&copy; David Seow 2022</p>
+    </footer>
+  </>
+);
+
+export default Layout;
